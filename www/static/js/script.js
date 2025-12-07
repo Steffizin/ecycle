f514dc7140eba5fb74cafe8b5beffb3e5f036260
Steffizin <dudi.steff@gmail.com>
2025-12-07 19:50:23 -0300

    final

--- www/static/js/script.js
+++ www/static/js/script.js
@@ -14,8 +14,8 @@ const map = L.map('map').setView([-23.620464473249893, -46.72967968674794], 16);
       className: 'orange-icon'
     });
 
-    L.marker([-23.620014136933627, -46.731924025049615], { icon: iconeLaranja }).addTo(map)
-      .bindPopup('Você está aqui!');
+    //L.marker([-23.620014136933627, -46.731924025049615], { icon: iconeLaranja }).addTo(map)
+    //  .bindPopup('Você está aqui!');
 
     L.marker([-23.621718724878775, -46.72845048246778], { icon: iconeVerde }).addTo(map)
       .bindPopup('Ponto de coleta: Escola Técnica Abdias');
